3d5f6c55d48d1f9f0e104db75e4520c36d90925e
Biruk Alemayehu <birukalem35@gmail.com>
2025-05-11 05:54:57 +0300

    finshed the login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,38 @@ const register = async (req, res) => {
   }
 };
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    if (!username || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.passeord);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    console.log("Error on the login user", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = {
   register,
